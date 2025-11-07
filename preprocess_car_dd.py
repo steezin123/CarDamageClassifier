ba46655f26471a94b3527a882a10ff79c0748d5b
prisriva <prisriva004@gmail.com>
2025-11-07 15:47:39 -0500

    data preprocessing test

---
+++ preprocess_car_dd.py
@@ -0,0 +1,7 @@
+python preprocess_car_dd.py \
+  --images_dir /path/to/images \
+  --annotations /path/to/annotations.json \
+  --out_dir ./car_dd_out \
+  --img_size 512 \
+  --save_numpy \
+  --save_flat_vectors
